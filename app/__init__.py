1085a5fabdce0ff7c89a807664e62cecff519905
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2023-06-22 17:00:14 -0400

    added hobbies and experience which is dynamically being generate in python.

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+    experience1={"experience": ['Vimeo', 'A Free Bird']}
+    hobbies1={"hobbies": ['Video Games', 'Calisthenics']}
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), experience1=experience1, hobbies1=hobbies1)
