8d050f6e7c9e78dd3e3a691594d263d96573d57c
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2023-06-23 19:10:42 -0400

    made changes to the init file. experience and hobbies page is dynamic and renders information based on the the fellow that is clicked on.

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,66 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+img = os.path.join('static', 'img')
 
 
 @app.route('/')
 def index():
 
-    experience1={"experience": ['Vimeo', 'A Free Bird']}
-    hobbies1={"hobbies": ['Video Games', 'Calisthenics']}
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), experience1=experience1, hobbies1=hobbies1)
+    fellow=['Reginald Jean Amedee', "Cassey Shao", "Eyob Dagnachew"]
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), fellow=fellow)
+
+
+
+
+@app.route('/<fellow>')
+def fellowPage(fellow):
+    fellow=request.args.get('fellow', fellow)
+    # if fellow == "Reginald":
+    #     hobbyImage1=os.path.join(img, 'elden_ring.png')
+    #     hobbyImage2=os.path.join(img, 'reggie_picture.jpg')
+    #     experience={"experience": ['Vimeo', 'A Free Bird']}
+    #     hobby1="Video Games"
+    #     hobby2="Hockey"
+    # elif fellow == "Cassey":
+    #     hobbyImage1=os.path.join(img, 'elden_ring.png')
+    #     hobbyImage2=os.path.join(img, 'reggie_picture.jpg')
+    #     experience={"experience": ['College', 'Local']}
+    #     hobby1="Coding"
+    #     hobby2="Art"
+    # elif fellow == "Eyob":
+    #     hobbyImage1=os.path.join(img, 'elden_ring.png')
+    #     hobbyImage2=os.path.join(img, 'reggie_picture.jpg')
+    #     experience={"experience": ['College', 'Local']}
+    #     hobby1="Drawing"
+    #     hobby2="Dancing"
+
+
+
+    return render_template('aboutMePage.html', fellow=fellow)
+
+@app.route('/<fellow>/experience')
+def experiencePage(fellow):
+    if fellow == "Reginald":
+        experience={"experience": ['Vimeo', 'A Free Bird']}
+        data="Reginald Jean Amedee"
+    elif fellow == "Cassey":
+        data="Cassey Shao"
+    elif fellow == "Eyob Dagnachew":
+        data="Eyob Dagnachew"
+
+    print(fellow)
+
+    return render_template('experiencePage.html', data=data, experience=experience)
+
+@app.route('/<fellow>/hobbies')
+def hobbiesPage(fellow):
+    if fellow == "Reginald":
+        data="Reginald Jean Amedee"
+    elif fellow == "Cassey":
+        data="Cassey Shao"
+    elif fellow == "Eyob Dagnachew":
+        data="Eyob Dagnachew"
+
+    return render_template('hobbiesPage.html', data=data)
