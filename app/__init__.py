050f3851f415ad73c190ba9f07936165ca3e493a
Cassey Shao <60023562+casseyshao@users.noreply.github.com>
2023-06-24 19:21:29 -0400

    Add photos of fellows as part of task 1
    * Add photos of fellows as part of task 1
    * Change CSS font-size from px to em

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,24 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+# Landing page data
+landing_data = [
+    {
+        "name": "Reginald Amedee",
+        "img": "./static/img/reginald.jpeg",
+    },
+    {
+        "name": "Eyob Dagnachew",
+        "img": "./static/img/eyob.jpeg",
+    },
+    {
+        "name": "Cassey Shao",
+        "img": "./static/img/cassey.JPG",
+    }
+]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    intro_message = "Welcome to our page! Click on a fellow to learn more about them!"
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), landing_data=landing_data, intro_message=intro_message)
