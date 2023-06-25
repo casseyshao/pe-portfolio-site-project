50a4745d5985b338ebf0ea8c4f0fe2c54ee81c56
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2023-06-25 09:59:48 -0400

    testing

--- app/__init__.py
+++ app/__init__.py
@@ -24,4 +24,5 @@ landing_data = [
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    intro_message = "Welcome to our page! Click on a fellow to learn more about them!"
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), landing_data=landing_data, intro_message=intro_message)
