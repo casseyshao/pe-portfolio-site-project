6ce813af64f37fcf799820648c51a98bfb29fb1b
Cassey Shao <60023562+casseyshao@users.noreply.github.com>
2023-06-27 15:47:15 -0400

    Add map of all locations visited by fellows
    * Add map of all locations visited by fellows
    * Add map legend

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import folium
 
 load_dotenv()
 app = Flask(__name__)
@@ -12,23 +13,115 @@ landing_data = [
     {
         "name": "Reginald Amedee",
         "img": "./static/img/reginald.jpeg",
+        "marker_color": "green",
+        "style": "pin1",
+        "places": [
+            {
+                "coord": [43.6532, -79.3832],
+                "name": "Toronto"
+            },
+            {
+                "coord": [37.4415, 25.3667],
+                "name": "Mykonos"
+            },
+            {
+                "coord": [23.1136, -82.3666],
+                "name": "Cuba"
+            },
+            {
+                "coord": [10.6918, -61.2225],
+                "name": "Trinidad"
+            },
+            {
+                "coord": [36.3932, 25.4615],
+                "name": "Santorini"
+            },
+            {
+                "coord": [64.9631, -19.0208],
+                "name": "Iceland"
+            },
+            {
+                "coord": [18.1096, 77.2975],
+                "name": "Jamaica"
+            }
+        ],
     },
     {
         "name": "Eyob Dagnachew",
         "img": "./static/img/eyob.jpeg",
+        "marker_color": "red",
+        "style": "pin2",
+        "places": [
+            {
+                "coord": [43.7615, -79.4111],
+                "name": "Canada"
+            },
+            {
+                "coord": [9.1450, 40.4897],
+                "name": "Ethiopia"
+            },
+            {
+                "coord": [52.3555, -1.1743],
+                "name": "England"
+            }
+        ],
     },
     {
         "name": "Cassey Shao",
         "img": "./static/img/cassey.JPG",
+        "marker_color": "blue",
+        "style": "pin3",
+        "places": [
+            {
+                "coord": [43.6205, -79.5132],
+                "name": "Toronto"
+            },
+            {
+                "coord": [48.8566, 2.3522],
+                "name": "Paris"
+            },
+            {
+                "coord": [38.7223, 9.1393],
+                "name": "Lisbon"
+            },
+            {
+                "coord": [43.7696, 11.2558],
+                "name": "Florence"
+            },
+            {
+                "coord": [37.9838, 23.7275],
+                "name": "Athens"
+            },
+            {
+                "coord": [39.9042, 116.4074],
+                "name": "Beijing"
+            },
+            {
+                "coord": [22.5090, -78.4070],
+                "name": "Cayo Coco"
+            }
+        ],
     }
 ]
 
+def build_map():
+    my_map = folium.Map()
+
+    # Add markers for each person
+    for person in landing_data:
+        for p in person["places"]:
+            folium.Marker(p["coord"], popup = p["name"], icon=folium.Icon(color=person["marker_color"], icon="circle", prefix="fa")).add_to(my_map)
+
+    my_map = my_map._repr_html_()
+    return my_map
 
 @app.route('/')
 def index():
-
+    my_map = build_map()
     intro_message = "Welcome to our page! Click on a fellow to learn more about them!"
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), landing_data=landing_data, intro_message=intro_message)
+    map_title = "A map of all the places that we have been to:"
+    map_desc = "Reginald (Green), Eyob (Red), Cassey (Blue)"
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), landing_data=landing_data, intro_message=intro_message, my_map=my_map, map_title=map_title, map_desc=map_desc)
 
 @app.route('/<fellow>')
 def fellowPage(fellow):
