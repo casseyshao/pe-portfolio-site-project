0a5a4ce744784fc02c6c4cc1d614744546d58ea1
Cassey Shao <casseys0000@gmail.com>
2023-07-13 15:39:53 -0400

    Update script to redeploy site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-systemctl stop /etc/systemd/system/myportfolio.service
+systemctl stop myportfolio
 cd pe-portfolio-site-project-fork
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
+systemctl start myportfolio
+systemctl enable myportfolio
