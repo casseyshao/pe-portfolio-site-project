77ccf38b5eb8cadace85cf3f1d3a1d0e2d1a8ac9
root <eyobdagnahew23@gmail.com>
2023-07-16 13:41:16 -0400

    all necessary edits to add a database to portfolio website as well as adding GET and POST endpoints with tests

---
+++ curl-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+
+
+
+
+LIST=$(curl  http://localhost:5000/api/timeline_post)
+
+
+ENTRY_COUNT=$(echo "$LIST" | jq '.timeline_posts | length')
+
+
+echo "this is a test  $ENTRY_COUNT"
+# Example command to list files
+if [ $LIST -ne 400 ]; then
+    echo "Command ran successfully"
+else
+    echo "Command encountered an error"
+fi
+
+
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=test&email=test@gmail.com&content=just added a database'
+
+LIST2=$(curl  http://localhost:5000/api/timeline_post)
+ENTRY_COUNT2=$(echo "$LIST2" | jq '.timeline_posts | length')
+
+
+
+if [ $ENTRY_COUNT2 -eq  $((ENTRY_COUNT+1)) ]; then
+	echo "Get request works"
+
+else
+	echo "Get request failure"
+fi
+
+
