55d9c62f7490d2bff31215bc2993735875a8f64c
Reginald Amedee <jeantechnical1992@gmail.com>
2023-07-16 19:42:49 +0000

    made changes to redeploy script.

--- redeploy-application.sh
+++ redeploy-application.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-tmux kill-server
-
-cd /pe-portfolio-site-project 
+cd /root/pe-portfolio-site-project 
 
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
-
-tmux new -s initiateWebsite
-
-
-
 
 
