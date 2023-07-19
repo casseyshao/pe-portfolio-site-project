fe6f80c69a4accb5b645d62fafbd7ff6fa3075db
Aerin Brown <aerinebrown04@gmail.com>
2023-07-19 12:10:05 -0700

    feat: Flask app testing

---
+++ tests/test_app.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+os.environ["TESTING"] = "true"
+
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+    
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<title>MLH Fellow</title>" in html
+
+    def test_timeline(self):
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 0
+        
+    def test_malformed_timeline_post(self):
+        response = self.client.post("/api/timeline_post", data={"email":"john@example.com", "content":"Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
+        
+        response = self.client.post("/api/timeline_post", data={"name":"John Doe", "email": "john@example.com", "content":""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        response = self.client.post("/api/timeline_post", data={"name":"John Doe", "email":"not-an-email", "content":"Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
