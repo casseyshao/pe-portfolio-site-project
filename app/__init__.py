961834afb1f5960189b69d6e350c14c6612f8868
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2023-07-25 11:58:41 -0400

    made portfolio website web responsive and removed eyob and cassey from website

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,9 @@ app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+
+
 if os.getenv("TESTING") == "true":
     print("Running in test mode")
     mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
@@ -95,62 +98,6 @@ landing_data = [
                 "name": "Jamaica"
             }
         ],
-    },
-    {
-        "name": "Eyob Dagnachew",
-        "img": "./static/img/eyob.jpeg",
-        "marker_color": "red",
-        "style": "pin2",
-        "places": [
-            {
-                "coord": [43.7615, -79.4111],
-                "name": "Canada"
-            },
-            {
-                "coord": [9.1450, 40.4897],
-                "name": "Ethiopia"
-            },
-            {
-                "coord": [52.3555, -1.1743],
-                "name": "England"
-            }
-        ],
-    },
-    {
-        "name": "Cassey Shao",
-        "img": "./static/img/cassey.JPG",
-        "marker_color": "blue",
-        "style": "pin3",
-        "places": [
-            {
-                "coord": [43.6205, -79.5132],
-                "name": "Toronto"
-            },
-            {
-                "coord": [48.8566, 2.3522],
-                "name": "Paris"
-            },
-            {
-                "coord": [38.7223, 9.1393],
-                "name": "Lisbon"
-            },
-            {
-                "coord": [43.7696, 11.2558],
-                "name": "Florence"
-            },
-            {
-                "coord": [37.9838, 23.7275],
-                "name": "Athens"
-            },
-            {
-                "coord": [39.9042, 116.4074],
-                "name": "Beijing"
-            },
-            {
-                "coord": [22.5090, -78.4070],
-                "name": "Cayo Coco"
-            }
-        ],
     }
 ]
 
@@ -166,9 +113,7 @@ def build_map():
     return my_map
 
 pic_data = {
-"Reginald Amedee": "./static/img/reginald.jpeg",
-"Eyob Dagnachew": "./static/img/eyob.jpeg",        
-"Cassey Shao": "./static/img/cassey.JPG",
+"Reginald Amedee": "./static/img/reginald.jpeg"
 }
 
 
@@ -180,22 +125,6 @@ about_me= {
     year to pursue this passion full time! 
     I have experience with a variety of technologies and 
     I love to build things. Always looking to collaborate!""",
-    "Cassey Shao": """ About me: Hi my name is Cassey! I am a new grad computer engineering student
-    from University of Toronto! I am interested in building good software! 
-    I have experience with building software from school, internships, and hackathons! 
-    Outside of this, I love being active! I like hiking, going on long walks in downtown, and playing sports!""",
-    "Eyob Dagnachew": """Hello there! My name is Eyob, I'm an incoming junior at 
-    Carnegie Mellon University in Pittsburgh! I love trying to find new ways to apply creativity
-    to make something new in the world! One of those ways is through coding which is something I've
-    been doing for the past couple years through internships, research, and hackathons! other than tech
-    I'm usually trying to find some more artistic outlets for my creativity, resulting in me
-    having a camera bag, sketchbook and laptop in my backpack almost constantly!  """,
-    "Eyob Dagnachew": """Hello there! My name is Eyob, I'm an incoming junior at 
-    Carnegie Mellon University in Pittsburgh! I love trying to find new ways to apply creativity
-    to make something new in the world! One of those ways is through coding which is something I've
-        been doing for the past couple years through internships, research, and hackathons! other than tech
-        I'm usually trying to find some more artistic outlets for my creativity, resulting in me
-        having a camera bag, sketchbook and laptop in my backpack almost constantly!  """
 }
 
 @app.route('/')
@@ -223,42 +152,17 @@ def experiencePage(fellow):
 'Lead Developer on a proprietary solution that will save over $5000 a year', 'Built out database models and backend using Django and Python according to system design requirements.'], "Date": "Feb 2023 - June 2023"}, {"Company" : "B.D.P.A", "Role": "Technical Instructor", "JobDescription": ['Demonstrated an exceptional ability to teach and communicate complex coding concepts to students of varying skill levels, resulting in hihgly-engaged and knowledable learners.',
 'Recreated popular website layouts with students by utilizing replit.com integrated development environment.'], "Date": "Feb 2023 - Aug 2023"}]
         data="Reginald Amedee"
-    elif fellow == "Cassey Shao":
-        experience=[{"Company" : "Amazon", "Role": "Software Engineer Intern", "JobDescription": ['Designed and worked on a command line tool to automate calculating Time-to-FirstResponse metric information for resolver groups to track their performance with responding to external requests.',
-'Abstracted models and encapsulated logic to future-proof the software and make it reusable by other teams', 'Wrote unit tests to test the functionality of the components in the command line tool.'], "Date": "May 2022 - Aug 2022"}, {"Company" : "University of Toronto", "Role": "Teaching Assistant", "JobDescription": ['Course: CSC343 Introduction to Databases',
-'Department(Dept): Dept. of Computer Science', 'Answered questions and graded evaluations containing relational data model, database design, and SQL concepts.'], "Date": "Jan 2022 - May 2022"}]
-        data="Cassey Shao"
-    elif fellow == "Eyob Dagnachew":
-        experience=[{"Company" : "Fluence", "Role": "Data Science Modeling Intern", "JobDescription": ['Re-organized test scripts into implementation tests and behavior tests, and increased coverage of both categories of tests.',
-'Implemented data pipeline for site-specific temperature and dispatch data from two sites.', 'Evaluated existing model performance against site data, and determined if there is site-specific bias in the existing model.', 'Implemented a Long-Short Term Model to improve prediction accuracy for individual sites'], "Date": 'June 2022 - Aug 2022'}]
-        data="Eyob Dagnachew"
-
     return render_template('experiencePage.html', data=data, experience=experience)
 
 @app.route('/<fellow>/hobbies')
 def hobbiesPage(fellow):
     hobbyImage1=os.path.join(hobbyImageDir, 'elden_ring.png')
     hobbyImage2=os.path.join(hobbyImageDir, 'pic7.png')
-    hikingImage=os.path.join(hobbyImageDir, "hiking.webp")
-    bookImage=os.path.join(hobbyImageDir, "books.webp")
-    travelImage=os.path.join(hobbyImageDir, "travel.jpg")
-    eyobPhotography=os.path.join(hobbyImageDir, "eyobPhotography.webp")
-    digitalArt=os.path.join(hobbyImageDir, "digitalArt.webp")
-    lightWriting=os.path.join(hobbyImageDir, "lightWriting.avif")
 
     if fellow == "Reginald Amedee":
         data="Reginald Amedee"
         hobbies=[{"Hobby_Blurb" : "I love to play video games. My favorite genre of video games are RPGs. Currently playing Diablo 4. I am also on my third playthrough for Elden Ring!", "Hobby_Image": hobbyImage1},
                 {"Hobby_Blurb": "I enjoy calisthenics, which is a form of exercise that prioritizes body movements such as push ups, sit ups, pull ups, etc... In fact, I love it so much that I created an app to track your calisthenics progressions and movements called StrengthTracker.", "Hobby_Image": hobbyImage2},]
-    elif fellow == "Cassey Shao":
-        data="Cassey Shao"
-        hobbies=[{"Hobby_Blurb" : "I enjoy reading everything from non-fiction to novels to poems to plays and more! I made it a goal this year to read 12 books.", "Hobby_Image": bookImage},
-                {"Hobby_Blurb": "I enjoy being outside and walking! One of my favourite hikes was Panorama Ridge in BC, Canada!", "Hobby_Image": travelImage}, {"Hobby_Blurb": "I love being on the road and having new experiences and trying new foods and meeting people! It is a dream to be able to travel a lot in my life!", "Hobby_Image": hikingImage}]
-    elif fellow == "Eyob Dagnachew":
-        hobbies=[{"Hobby_Blurb" : "I always enjoy putting something from my imagination and challenging myself into making it as real as possible with the basic theories of art.", "Hobby_Image": digitalArt},
-                {"Hobby_Blurb": "I love photography because it challenges me to taking something that already exists that I already like and engage with it in a new way by trying to represent it in a new was through the lens.", "Hobby_Image": eyobPhotography}, {"Hobby_Blurb": "I like writing because it fills the gap of things that i cant bring to life in writing by brining them to life with my words instead, allowing me to delved even further to my imagination.", "Hobby_Image": lightWriting}]
-        data="Eyob Dagnachew"
-
     return render_template('hobbiesPage.html', data=data, hobbies=hobbies)
 
 
@@ -312,6 +216,9 @@ def timeline():
 
     return render_template('timeline.html', title="timeline", data=data)
 
+@app.route("/<id>/delete", methods=["DELETE"])
+def delete():
+    id = TimelinePost.select()
 
 if os.getenv("TESTING") == "true":
     print("Running in test mode")
