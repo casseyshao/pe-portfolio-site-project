22404f3f0be20789169eecfe11ba5d26a95ee99f
Reggie-Amedee-Tech <jeantechnical1992@gmail.com>
2023-07-28 18:07:38 -0400

    changed redeployment script

--- redeploy-application.sh
+++ redeploy-application.sh
@@ -6,7 +6,7 @@ git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 docker compose -f docker-compose.prod.yml down
 
